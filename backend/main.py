735c8675a3b2349958415f991d8643251895f97c
Inanology <abcoaj888@gmail.com>
2026-07-07 14:39:26 +0800

    fix: add www.xfinlab.com to CORS

--- backend/main.py
+++ backend/main.py
@@ -41,7 +41,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://xfinlab.com", "http://localhost:3001", "http://localhost:3000"],
+    allow_origins=["https://xfinlab.com", "https://www.xfinlab.com", "http://localhost:3001", "http://localhost:3000"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
